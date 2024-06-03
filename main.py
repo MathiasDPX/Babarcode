d3e580ea63c936998d8a588032ae85230323aa49
MathiasDPX <mathias@dupeux.net>
2024-06-03 21:27:00 +0200

    Add comments and config

--- main.py
+++ main.py
@@ -1,33 +1,35 @@
-import pydirectinput
+import pydirectinput # pyautogui doesnt work
 from time import sleep
 import keyboard
 
-pressed = []
+walking_duration = 0.1
+leftclick_key = "n"
 
 def holdKey(key, time):
     pydirectinput.keyDown(key)
     sleep(time)
     pydirectinput.keyUp(key)
-
+ 
+# Zzz... boring
 def resolve(combine):
     if combine == "01":
         print("Drop item")
         pydirectinput.press("a")
     elif combine == "02":
         print("Walk forward")
-        holdKey("w", 0.1)
+        holdKey("w", walking_duration)
     elif combine == "03":
         print("Open inventory")
         pydirectinput.press("e")
     elif combine == "07":
         print("Walk left")
-        holdKey("q", 0.1)
+        holdKey("q", walking_duration)
     elif combine == "08":
         print("Walk backward")
-        holdKey("s", 0.1)
+        holdKey("s", walking_duration)
     elif combine == "09":
         print("Walk right")
-        holdKey("d", 0.1)
+        holdKey("d", walking_duration)
     elif combine == "04":
         print("Jump")
         pydirectinput.press("space")
@@ -47,22 +49,27 @@ def resolve(combine):
         print("Esc")
         pydirectinput.press("esc")
     elif combine == "12":
-        if keyboard.is_pressed("n"):
-            pydirectinput.keyUp("n")
+        # pydirectinput doesn't have mouseLeftUp nor mouseLeftDown
+        if keyboard.is_pressed(leftclick_key):
+            pydirectinput.keyUp(leftclick_key)
         else:
-            pydirectinput.keyDown("n")
+            pydirectinput.keyDown(leftclick_key)
     else:
         print(f"Can't resolve {combine}")
 
+# Useful
+pressed = []
 while True:
     key = keyboard.read_event()
     
     if key.event_type != keyboard.KEY_DOWN:
         continue
 
+    # so you can type in chat
     if key.name.isnumeric():
         pressed.append(key.name)
 
+    # Enter or any terminator
     if key.name == "enter":
         combine = "".join(pressed)
         resolve(combine)
