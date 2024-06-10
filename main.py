1985a64d4eb5c6f58c075091a41cc858d258345e
MathiasDPX <mathias@dupeux.net>
2024-06-10 20:26:25 +0200

    Add Stats

--- main.py
+++ main.py
@@ -13,49 +13,55 @@ def holdKey(key, time):
     pydirectinput.keyDown(key)
     sleep(time)
     pydirectinput.keyUp(key)
- 
+
+def increment(key:str, value:int):
+    try:
+        stats[key] += value
+    except KeyError:
+        stats[key] = value
+
 # Zzz... boring
 def resolve(combine):
     if combine == "01":
-        stats['Left'] += 1
+        increment('Left', 1)
         pydirectinput.press("a")
     elif combine == "02":
-        stats['Forward'] += 1
+        increment("Forward", 1)
         holdKey("w", walking_duration)
     elif combine == "03":
-        stats['Inventory'] += 1
+        increment("Inventory", 1)
         pydirectinput.press("e")
     elif combine == "07":
-        stats['Right'] += 1
+        increment("Left", 1)
         holdKey("q", walking_duration)
     elif combine == "08":
-        stats['Backward'] += 1
+        increment("Backward", 1)
         holdKey("s", walking_duration)
     elif combine == "09":
-        stats['Right'] += 1
+        increment("Right", 1)
         holdKey("d", walking_duration)
     elif combine == "04":
-        stats['Jump'] += 1
+        increment("Jump", 1)
         pydirectinput.press("space")
     elif combine == "05":
-        stats['LMB'] += 1
+        increment("LMB", 1)
         pydirectinput.press(lmb)
     elif combine == "06":
-        stats['RMB'] += 1
+        increment("RMB", 1)
         pydirectinput.press(rmb)
     elif combine == "10":
-        stats['Ctrl'] += 1
+        increment("Ctrl", 1)
         pydirectinput.press("ctrl")
     elif combine == "11":
-        stats['Shift'] += 1
+        increment("Shift", 1)
         pydirectinput.press("shift")
     elif combine == "13":
-        stats['Escape'] += 1
+        increment("Esc", 1)
         pydirectinput.press("esc")
     elif combine == "12":
         # pydirectinput doesn't have mouseLeftUp nor mouseLeftDown
         if keyboard.is_pressed(lmb):
-            stats['Long LMB'] += 1
+            increment("Long LMB", 1)
             pydirectinput.keyUp(lmb)
         else:
             pydirectinput.keyDown(lmb)
@@ -81,6 +87,7 @@ try:
             resolve(combine)
             pressed = []
 except KeyboardInterrupt:
+    print("--STATS:")
     for stat, value in stats.items():
         print(f"{stat}: {value}")
     exit()
